d1a6bb14722ac930f3aa497fa6beb53e2bfde8a7
Olivia <denbuw@gmail.com>
2019-06-19 12:06:33 +0200

    - Started implementing markup for localization - Trying to figure out how to use only ONE resx file per language

--- BurkinafasoSite/Controllers/HomeController.cs
+++ BurkinafasoSite/Controllers/HomeController.cs
@@ -5,13 +5,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BurkinafasoSite.Models;
+using Microsoft.Extensions.Localization;
+using BurkinafasoSite.Resources;
+using System.Globalization;
 
 namespace BurkinafasoSite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IStringLocalizer<SharedResource> _localizer;
+
+        public HomeController(IStringLocalizer<SharedResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        //public string GetLocalizedString()
+        //{
+        //    return _localizer["My localized string"];
+        //}
+
         public IActionResult Index()
         {
+            //var test = new SharedViewLocalizer();
+            //test.GetLocalizedString("Btn.Save");
+            _localizer.WithCulture(new CultureInfo("en"));
+            var btnSave = _localizer["Btn-save"];
             return View();
         }
 
--- BurkinafasoSite/Startup.cs
+++ BurkinafasoSite/Startup.cs
@@ -12,6 +12,11 @@ using Microsoft.Extensions.DependencyInjection;
 using BurkinafasoSite.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+using BurkinafasoSite.Resources;
 
 namespace BurkinafasoSite
 {
@@ -34,16 +39,59 @@ namespace BurkinafasoSite
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            //Localization
+            //Andrew
+            //services.AddLocalization(opts => { opts.ResourcesPath = "Resources"; });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc()
+                 //Localization
+
+                 //Andrew
+                 //.AddViewLocalization(
+                 //   LanguageViewLocationExpanderFormat.Suffix,
+                 //   opts => { opts.ResourcesPath = "Resources"; })
+
+                //"Less than 5min"
+                .AddDataAnnotationsLocalization(options => 
+                    options.DataAnnotationLocalizerProvider = (type, factory) =>
+                        factory.Create(typeof(SharedResource)))
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddScoped<SharedViewLocalizer>();
+
+            //Localization: Andrew
+            //.AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, opts => { opts.ResourcesPath = "Resources"; })
+            //.AddDataAnnotationsLocalization();
+
+            //Localization: Andrew
+            //För att välja språk
+            services.Configure<RequestLocalizationOptions>(
+                opts =>
+                {
+                    var supportedCultures = new List<CultureInfo>
+                    {
+                        new CultureInfo("en-GB"),
+                        new CultureInfo("en-US"),
+                        new CultureInfo("en"),
+                        new CultureInfo("fr-FR"),
+                        new CultureInfo("fr"),
+                        new CultureInfo("sv")
+                    };
+
+                    opts.DefaultRequestCulture = new RequestCulture("en-GB");
+                    // Formatting numbers, dates, etc.
+                    opts.SupportedCultures = supportedCultures;
+                    // UI strings that we have localized.
+                    opts.SupportedUICultures = supportedCultures;
+                });
+
             //Identity
             services.AddDbContext<ApplicationDbContext>(
-                options => options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
-            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,6 +111,11 @@ namespace BurkinafasoSite
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
+
+            //Localization: Andrew
+            //För att välja språk
+            var options = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(options.Value);
 
             app.UseMvc(routes =>
             {
