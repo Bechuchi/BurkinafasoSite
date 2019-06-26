619201d6f2078303fb130715a7fb70eb833af0c2
Olivia <denbuw@gmail.com>
2019-06-26 09:10:12 +0200

    - Added markup in AccountController for Login() and Register()

--- BurkinafasoSite/Controllers/AccountController.cs
+++ BurkinafasoSite/Controllers/AccountController.cs
@@ -2,20 +2,102 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BurkinafasoSite.Data;
+using BurkinafasoSite.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BurkinafasoSite.Controllers
 {
+    [Authorize]
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager,
+                                 SignInManager<ApplicationUser> signInManager)
         {
-            return View();
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
 
+        #region Login
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login()
+        {
+            // Clear the existing external cookie to ensure a clean login process
+            await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
+
+            return View();
+        }
+        
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //TODO
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, set lockoutOnFailure: true
+                var result = await _signInManager.PasswordSignInAsync(viewModel.Email, viewModel.Password, viewModel.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(viewModel);
+                }
+            }
+
+            return View(viewModel);
+        }
+        #endregion
+
+        #region Register
+        [HttpGet]
+        [AllowAnonymous]
+        //TODO: Kolla om den ska vara task
         public IActionResult Register()
         {
             return View();
         }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var newUser = new ApplicationUser { UserName = viewModel.Email, Email = viewModel.Email };
+                var result = await _userManager.CreateAsync(newUser, viewModel.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(newUser, "Regular");
+                    //TODO: Ev fixa _logger
+                    //_logger.LogInformation("User created a new account with password.");
+
+                    //TODO: Ev fixa e-mail confirmation
+                    //var code = await _userManager.GenerateEmailConfirmationTokenAsync(newUser);
+                    //var callbackUrl = Url.EmailConfirmationLink(newUser.Id, code, Request.Scheme);
+                    //await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+                    await _signInManager.SignInAsync(newUser, isPersistent: false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                //TODO: Ev AddErrors
+            }
+
+            return View(viewModel);
+        }
+        #endregion
     }
 }
